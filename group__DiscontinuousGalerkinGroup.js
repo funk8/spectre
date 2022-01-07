92c8d25f6bbb28538c4dfa2ff57e3e57761967c2
nilsdeppe <nilsdeppe@users.noreply.github.com>
2022-01-07 23:01:01 +0000

    deploy: e09e41f9eb3b0ff7bb0fbf5d6cc5fa3b7eea4e34

---
+++ group__DiscontinuousGalerkinGroup.js
@@ -0,0 +1,59 @@
+var group__DiscontinuousGalerkinGroup =
+[
+    [ "Limiters::Tags::LimiterCommunicationTag< Metavariables >", "structLimiters_1_1Tags_1_1LimiterCommunicationTag.html", null ],
+    [ "Limiters::Actions::Limit< Metavariables >", "structLimiters_1_1Actions_1_1Limit.html", null ],
+    [ "Limiters::Actions::SendData< Metavariables >", "structLimiters_1_1Actions_1_1SendData.html", null ],
+    [ "dg::NumericalFluxes::Hll< System >", "structdg_1_1NumericalFluxes_1_1Hll.html", [
+      [ "LargestIngoingSpeed", "structdg_1_1NumericalFluxes_1_1Hll_1_1LargestIngoingSpeed.html", null ],
+      [ "LargestOutgoingSpeed", "structdg_1_1NumericalFluxes_1_1Hll_1_1LargestOutgoingSpeed.html", null ]
+    ] ],
+    [ "dg::NumericalFluxes::LocalLaxFriedrichs< System >", "structdg_1_1NumericalFluxes_1_1LocalLaxFriedrichs.html", [
+      [ "MaxAbsCharSpeed", "structdg_1_1NumericalFluxes_1_1LocalLaxFriedrichs_1_1MaxAbsCharSpeed.html", null ]
+    ] ],
+    [ "dg::SimpleMortarData< TemporalId, LocalVars, RemoteVars >", "classdg_1_1SimpleMortarData.html", [
+      [ "SimpleMortarData", "classdg_1_1SimpleMortarData.html#a8fdfdfd3fb2cdd9ee34b85e0f25fa2ec", null ],
+      [ "extract", "classdg_1_1SimpleMortarData.html#ae6e0659e7b9533628e1497df13e322b0", null ],
+      [ "integration_order", "classdg_1_1SimpleMortarData.html#aa72b4ad91c3c0a354b9a7d86bf816937", null ],
+      [ "integration_order", "classdg_1_1SimpleMortarData.html#a345fd0135b72ba2bd7826c62a2751a88", null ],
+      [ "local_data", "classdg_1_1SimpleMortarData.html#a270ac32d357b077da48d4442b6d66219", null ],
+      [ "local_insert", "classdg_1_1SimpleMortarData.html#a2dd90a58d5f6f73b2633bd68904dacfe", null ],
+      [ "remote_data", "classdg_1_1SimpleMortarData.html#a62fcbe287e5a13950bf81e15509127be", null ],
+      [ "remote_insert", "classdg_1_1SimpleMortarData.html#a0bbde83de229766aab8f3ba8606df69e", null ]
+    ] ],
+    [ "Tags::SimpleMortarData< TemporalId, LocalData, RemoteData >", "structTags_1_1SimpleMortarData.html", null ],
+    [ "Tags::Mortars< Tag, VolumeDim >", "structTags_1_1Mortars.html", null ],
+    [ "Tags::MortarSize< Dim >", "structTags_1_1MortarSize.html", null ],
+    [ "Filters::Exponential< FilterIndex >", "classFilters_1_1Exponential.html", [
+      [ "Alpha", "structFilters_1_1Exponential_1_1Alpha.html", null ],
+      [ "DisableForDebugging", "structFilters_1_1Exponential_1_1DisableForDebugging.html", null ],
+      [ "HalfPower", "structFilters_1_1Exponential_1_1HalfPower.html", null ],
+      [ "filter_matrix", "classFilters_1_1Exponential.html#a90e2e50edbe22547957ace279f5598e8", null ]
+    ] ],
+    [ "dg::Actions::Filter< FilterType, tmpl::list< TagsToFilter... > >", "classdg_1_1Actions_1_1Filter_3_01FilterType_00_01tmpl_1_1list_3_01TagsToFilter_8_8_8_01_4_01_4.html", null ],
+    [ "dg::Events::ObserveErrorNorms< ObservationValueTag, tmpl::list< Tensors... >, ArraySectionIdTag >", "classdg_1_1Events_1_1ObserveErrorNorms_3_01ObservationValueTag_00_01tmpl_1_1list_3_01Tensors_8_8017a6189979d55ef1a49af85975e9c00.html", [
+      [ "SubfileName", "structdg_1_1Events_1_1ObserveErrorNorms_3_01ObservationValueTag_00_01tmpl_1_1list_3_01Tensors_8_ed496315798a0cb479b0192bbc0130fc.html", null ],
+      [ "needs_evolved_variables", "classdg_1_1Events_1_1ObserveErrorNorms_3_01ObservationValueTag_00_01tmpl_1_1list_3_01Tensors_8_8017a6189979d55ef1a49af85975e9c00.html#a38664ac6c8aa7738732e6d6689813e91", null ]
+    ] ],
+    [ "dg::Events::ObserveFields< VolumeDim, ObservationValueTag, tmpl::list< Tensors... >, tmpl::list< NonTensorComputeTags... >, tmpl::list< AnalyticSolutionTensors... >, ArraySectionIdTag, tmpl::list< NonSolutionTensors... > >", "classdg_1_1Events_1_1ObserveFields_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_1list_3_0132d3427bdf41385d9d5c6a7077413ac1.html", [
+      [ "CoordinatesFloatingPointType", "structdg_1_1Events_1_1ObserveFields_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_1list_3_0d5a72648231ec1951759dc059addfacc.html", null ],
+      [ "FloatingPointTypes", "structdg_1_1Events_1_1ObserveFields_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_1list_3_0ffdd103bd22c5855116627196bdc602d.html", null ],
+      [ "InterpolateToMesh", "structdg_1_1Events_1_1ObserveFields_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_1list_3_003f895780e2f329b92a4596c4ad31805.html", null ],
+      [ "SubfileName", "structdg_1_1Events_1_1ObserveFields_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_1list_3_0d2338e1199ee188c4575af2ecfdcc749.html", null ],
+      [ "VariablesToObserve", "structdg_1_1Events_1_1ObserveFields_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_1list_3_0d2a5a83587d710dd57861ec876c42b2f.html", null ],
+      [ "needs_evolved_variables", "classdg_1_1Events_1_1ObserveFields_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_1list_3_0132d3427bdf41385d9d5c6a7077413ac1.html#aba3b2ce4b37e8657a11085ea651f76a6", null ]
+    ] ],
+    [ "dg::Events::ObserveVolumeIntegrals< VolumeDim, ObservationValueTag, tmpl::list< Tensors... >, tmpl::list< NonTensorComputeTags... >, ArraySectionIdTag >", "classdg_1_1Events_1_1ObserveVolumeIntegrals_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_117bbe1a13a15c77c397dbc9477bff9b5.html", [
+      [ "SubfileName", "structdg_1_1Events_1_1ObserveVolumeIntegrals_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_ce2042c0210c155d200e7a754122b866.html", null ],
+      [ "needs_evolved_variables", "classdg_1_1Events_1_1ObserveVolumeIntegrals_3_01VolumeDim_00_01ObservationValueTag_00_01tmpl_1_117bbe1a13a15c77c397dbc9477bff9b5.html#a38f1a99fc293a0353c64f5ebf1f1bf10", null ]
+    ] ],
+    [ "dg::Formulation", "group__DiscontinuousGalerkinGroup.html#ga102ad013b7e4298b4c14c52a39bcd8da", null ],
+    [ "dg::has_received_from_all_mortars", "group__DiscontinuousGalerkinGroup.html#gaa966d9760b419f933e2e39c32f326715", null ],
+    [ "dg::lift_flux", "group__DiscontinuousGalerkinGroup.html#ga72fe2658e89afabf1d99954773b30521", null ],
+    [ "dg::lift_flux", "group__DiscontinuousGalerkinGroup.html#gab3c27a9baa2c03a01b200652e919b34c", null ],
+    [ "dg::metric_identity_det_jac_times_inv_jac", "group__DiscontinuousGalerkinGroup.html#ga6c128051a8706e751383fbfed7d91241", null ],
+    [ "dg::metric_identity_jacobian_quantities", "group__DiscontinuousGalerkinGroup.html#ga6a3fde3477693771bf8a029dd8183e6f", null ],
+    [ "dg::mortar_mesh", "group__DiscontinuousGalerkinGroup.html#ga6fd98c38b052d23f6f1f7cf5868c5158", null ],
+    [ "dg::mortar_size", "group__DiscontinuousGalerkinGroup.html#gab297ce2a5916fc0078f4bedb04437bb9", null ],
+    [ "dg::project_from_mortar", "group__DiscontinuousGalerkinGroup.html#gaa8ac9cf09cb971f4a3039d654d4bb6e4", null ],
+    [ "dg::project_to_mortar", "group__DiscontinuousGalerkinGroup.html#ga78dd3d6006c20d1a2429caa8f5a42556", null ]
+];
